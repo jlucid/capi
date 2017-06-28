37ff790b9d6d44879ab4e971f647d7dded581cd2
jlucid <jerlucid@gmail.com>
2017-06-28 19:44:35 +0000

    Simplify singleRow.c and add error capture

--- singleRow.c
+++ singleRow.c
@@ -13,13 +13,21 @@ int main()
 
     handle = khpu(hostname,portnumber,usernamePassword);
 
-    while(1)
-        {
-            sleep(1);
-            singleRow = knk(3,ks((S)"ABC"),kf(10.0), ki(20));
-            result = k(handle,".u.upd", ks((S)"trade"),singleRow,(K)0);
-        }
+    singleRow = knk(3,ks((S)"ABC"),kf(10.0), kj(20));
+    // Perform single row insert, tickerplant will add timestamp column itself
+    result = k(handle,".u.upd", ks((S)"trade"),singleRow,(K)0);
 
+    // Capture network error
+    if(!result){
+      perror("Network Error\n");
+    }
+
+    // Capture error associated with failed insert
+    if(-128==result->t){
+      printf("Error %s\n",result->s);
+    }
+
+    r0(result);
     kclose(handle);
     return 0;
 }
