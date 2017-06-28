e913ff9c6a32ef7f785b2cb2f1045adf5c6fffff
jlucid <jerlucid@gmail.com>
2017-06-28 22:05:12 +0000

    Publish time in nano-seconds

--- rowswithtime.c
+++ rowswithtime.c
@@ -4,10 +4,10 @@
 #include<time.h>
 #include "k.h"
 
-I castTime(struct tm *x)
+J castTime(struct tm *x)
 {
-    return (I) ((60*x->tm_hour+x->tm_min)*60 + x->tm_sec)*1000;
-};
+    return (J) ((60*x->tm_hour+x->tm_min)*60 + x->tm_sec)*1000000000;
+}
 
 int main()
 {
@@ -16,29 +16,37 @@ int main()
     I portnumber = 5010;
     S hostname = "localhost";
     K result;
-    S usernamePassword="kdb:pass",symbols[]= {"ABC","DEF","GHI"};
+    S usernamePassword = "kdb:pass";
+    S symbols[]= {"ABC","DEF","GHI"};
     time_t currentTime;
     struct tm *ct;
 
     handle = khpu(hostname, portnumber, usernamePassword);
 
-    while(1)
+    K multipleRow=knk(4,ktn(KN,n),ktn(KS,n),ktn(KF,n),ktn(KJ,n));
+    time( &currentTime );
+    ct = localtime( &currentTime );
+
+    for(i=0; i<n; i++)
         {
-            sleep(1);
-            K multipleRow=knk(4,ktn(KT,n),ktn(KS,n),ktn(KF,n),ktn(KI,n));
-            time( &currentTime );
-            ct = localtime( &currentTime );
-
-            for(i=0; i<n; i++)
-                {
-                    kI(kK(multipleRow)[0])[i] = castTime(ct);
-                    kS(kK(multipleRow)[1])[i] = ss(symbols[i%n]);
-                    kF(kK(multipleRow)[2])[i] = 10.0*i;
-                    kI(kK(multipleRow)[3])[i] = i;
-                }
-            result = k(handle,".u.upd",ks((S)"trade"),multipleRow,(K)0);
+            kJ(kK(multipleRow)[0])[i] = castTime(ct);
+            kS(kK(multipleRow)[1])[i] = ss(symbols[i%n]);
+            kF(kK(multipleRow)[2])[i] = 10.0*i;
+            kJ(kK(multipleRow)[3])[i] = i;
         }
+    result = k(handle,".u.upd",ks((S)"trade"),multipleRow,(K)0);
 
+   // Capture network error
+    if(!result){
+      perror("Network Error\n");
+    }
+
+    // Capture error associated with failed insert
+    if(-128==result->t){
+      printf("Error %s\n",result->s);
+    }
+
+    r0(result);
     kclose(handle);
     return 0;
 }
