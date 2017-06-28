debce51721a8ed1b5d356bcd6c1224473dfcf983
jlucid <jerlucid@gmail.com>
2017-06-28 16:19:16 +0100

    Added return command for errors

--- mathLib.c
+++ mathLib.c
@@ -10,13 +10,13 @@ K dotProduct(K x,K y)
     //Check data types are consistent
     if(x->t != 9 || y->t != 9)
         {
-            krr("type");
+            return krr("type");
         }
 
     //Check vector lengths are equal
     if(x->n != y->n)
         {
-            krr("length");
+            return krr("length");
         }
 
     for(i=0; i<x->n; i++)
