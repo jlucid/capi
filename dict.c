050b90b4ca69944b66226fb1bb2a31b8d176a40e
jlucid <jerlucid@gmail.com>
2017-07-05 18:24:54 +0000

    dict.c: Add error capture

--- dict.c
+++ dict.c
@@ -13,7 +13,33 @@ int main()
 
     handle = khpu(hostname,portnumber,usernamePassword);
 
+    if(handle==0)
+        {
+            printf("Authentication error %d\n",handle);
+            return 0;
+        }
+
+    if(handle==-1)
+        {
+            printf("Connection error %d\n",handle);
+            return 0;
+        }
+
     x = k(handle,"`a`b`c!((1;2;3);(10.0;20.0);(`AB`CD`EF`GH))",(K)0);
+
+    if(!x)
+        {
+            perror("Network Error\n");
+            return 0;
+        }
+
+    if(-128==x->t)
+        {
+            printf("Error message returned : %s\n",x->s);
+            r0(x);
+	    return 0;
+        }
+
     printf("Dict type = %d\n",x->t);
     printf("Num elements = %lld (keys and values)\n",x->n);
 
@@ -44,6 +70,8 @@ int main()
     printf("values[1;] = %lf %lf       \n",values1[0],values1[1]);
     printf("values[2;] = %s %s %s %s   \n",values2[0],values2[1],values2[2],values2[3]);
 
+    r0(x);
+    r0(keys);
     kclose(handle);
     return 0;
 }
