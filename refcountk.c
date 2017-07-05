60ff3fc34d542feddc8788e02a7b02c6a9707cd6
jlucid <jerlucid@gmail.com>
2017-07-05 18:49:19 +0000

    refcountk.c: Add error capture and free memory

--- refcountk.c
+++ refcountk.c
@@ -1,7 +1,7 @@
 /* File name: refcountk.c */
 #include<stdio.h>
 #include"k.h"
-// Note that error capture has been removed
+
 int main()
 {
     I handle;
@@ -15,10 +15,38 @@ int main()
     printf("int value of a is %d\n",a->i);
 
     handle = khpu(hostname,portnumber,usernamePassword);
+
+    if(handle==0)
+        {
+            printf("Authentication error %d\n",handle);
+            return 0;
+        }
+
+    if(handle==-1)
+        {
+            printf("Connection error %d\n",handle);
+            return 0;
+        }
+
     response = k(handle,"{x+3i}",a,(K)0);
+
+    if(!response)
+        {
+            perror("Network Error\n");
+            return 0;
+        }
+
+    if(-128==response->t)
+        {
+            printf("Error message returned : %s\n",response->s);
+            r0(response);
+	    return 0;
+        }
    
     printf("response value is %d\n", response->i);
     printf("value of a is now %d\n", a->i);
+    
+    r0(response);
     kclose(handle);
     return 0;
 }
