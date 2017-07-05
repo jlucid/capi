3002b96cc4beb29eba8b52a9e9f0d851b42ce466
jlucid <jerlucid@gmail.com>
2017-07-05 17:40:23 +0000

    alivecheck.c:Add error capture + free result if error occurs

--- alivecheck.c
+++ alivecheck.c
@@ -1,7 +1,7 @@
 /* File name: alivecheck.c */
 #include<stdio.h>
 #include"k.h"
-// Note that error capture has been removed
+
 int main()
 {
     I handle;
@@ -11,9 +11,35 @@ int main()
     K result;
 
     handle = khpu(hostname,portnumber,usernamePassword);
-    result = k(handle,"2.0+3.0",(K)0);
+
+    if(handle==0)
+        {
+            printf("Authentication error %d\n",handle);
+            return 0;
+        }
+
+    if(handle==-1)
+        {
+            printf("Connection error %d\n",handle);
+            return 0;
+        }
 
     printf("Handle value is %d\n",handle);
+    result = k(handle,"2.0+3.0",(K)0);
+
+    if(!result)
+        {
+            perror("Network Error\n");
+            return 0;
+        }
+
+    if(-128==result->t)
+        {
+            printf("Error message returned : %s\n",result->s);
+            r0(result);
+	    return 0;
+        }
+
     printf("Value returned is %f\n",result->f);
 
     r0(result);
