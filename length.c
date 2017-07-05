76a1e618f49e6412e85fcc844fc4b0cb13fd35ea
jlucid <jerlucid@gmail.com>
2017-07-05 17:52:45 +0000

    lenght.c: Add error capture

--- length.c
+++ length.c
@@ -12,7 +12,33 @@ int main()
 
     handle = khpu(hostname,portnumber,usernamePassword);
 
+    if(handle==0)
+        {
+            printf("Authentication error %d\n",handle);
+            return 0;
+        }
+
+    if(handle==-1)
+        {
+            printf("Connection error %d\n",handle);
+            return 0;
+        }
+
     list = k(handle,"2*1 2 3f",(K)0);
+
+    if(!list)
+        {
+            perror("Network Error\n");
+            return 0;
+        }
+
+    if(-128==list->t)
+        {
+            printf("Error message returned : %s\n",list->s);
+            r0(list);
+	    return 0;
+        }
+
     printf("List length is %lld\n",list->n);
 
     r0(list);
